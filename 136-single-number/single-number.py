ad1c34269251de5267d64042328eea8e6b37c179
Nithish <53188715+NithishGovind@users.noreply.github.com>
2025-09-27 22:45:48 +0530

    Time: 4 ms (56.54%) | Memory: 15.3 MB (6.37%) - LeetSync

---
+++ 136-single-number/single-number.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        d = {}
+        for i in nums:
+            if i not in d:
+                d[i] = 0
+            d[i]+=1
+        for j,k in d.items():
+            if k==1:
+                return j
+
+           
